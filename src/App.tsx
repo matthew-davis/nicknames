8877ab9998dcbc6b40eb27f98fae0b7057a93d0a
Matthew Davis <matthew.davis@wittin.co.uk>
2019-05-26 23:37:42 +0100

    First pass on the nickname generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,48 @@
 import React                     from 'react';
-import * as _                    from 'lodash';
 import {Button, Grid, TextField} from '@material-ui/core';
 import {seedList}                from './seedList';
-import {arrangeInput, arrangeCadence}            from './utils';
+import {
+  arrangeInput,
+  arrangeCadence,
+  datamuseRequest,
+  random
+}                                from './utils';
 import './App.css';
 
-
-
-
 const App: React.FC = () => {
-  const [nickname, setNickname] = React.useState('');
-  let count: number = 0;
-
-  const apiCall = async (name: string, oneChar: string, twoChar: string, seed: string) => {
-    const initResponse = await fetch(`https://api.datamuse.com/words?rel_jjb=${seed}&sp=${oneChar}*`);
-    const response = await initResponse.json();
-    if (Object.keys(response).length === 0) {
-      if (count <= 5) {
-        count++;
-      } else {
-        setNickname('Special ' + name);
-        return false;
-      }
-      apiCall(name, oneChar, twoChar, seedList[Math.floor(Math.random()*seedList.length)]);
-    } else {
-      const best = _.filter(response, (name: any) => name.word.substring(0, 1) === twoChar);
-      if (best.length > 0) {
-        setNickname(_.startCase(_.camelCase(best[Math.floor(Math.random()*best.length)].word) + ' ' + name));
-        return false;
-      } else {
-        setNickname(_.startCase(_.camelCase(response[Math.floor(Math.random()*response.length)].word) + ' ' + name));
-        return false;
-      }
-    }
-  };
-
+  const [nickname, setNickname] = React.useState<string | false | undefined>('');
+  let nick: string | false | undefined = '';
 
   // Write that syllable needs a declaration file written for it
+  // Write a readme.md
 
-  // Sort Aesthetics
-  // Looking to set the placeholder text initial text 2px lower
   // Connect live
   // Check on mobile phone
   // Final aesthetics pass
 
-
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     let namesInput = arrangeInput(e.target.name.value);
-    arrangeCadence(namesInput['first']);
+    namesInput.first = arrangeCadence(namesInput['first']);
+    if ('second' in namesInput) {
+      namesInput.first = arrangeCadence(namesInput['second']);
+      // Urban Dictionary Request
 
-    // if namesInput has a key 'second'
-    // arrangeCadence(namesInput['second']);
-    // urbanDictRequest
-    // else
-    // datamuseRequest
+      // Including this so the project workd until I've connected the Urban Dictionary
+      const seed = random(seedList);
+      nick = await datamuseRequest(namesInput, seed, 0);
 
-    // Both then set Nickname with their value
-
-    const firstChar = e.target.name.value.charAt(0);
-    const firstTwoChar = e.target.name.value.substring(0, 1);
-    const seed = seedList[Math.floor(Math.random()*seedList.length)];
-    count = 0;
-    await apiCall(e.target.name.value, firstChar, firstTwoChar, seed);
+    } else {
+      const seed = random(seedList);
+      nick = await datamuseRequest(namesInput, seed, 0);
+    }
+    setNickname(nick);
   };
 
   return (
     <React.Fragment>
       <Grid container direction ={'row'} justify={'center'} alignItems={'flex-start'}>
-        <h1 className={'title'}>Get A Proper Manly Nickname!</h1>
+        <h1 className={'title'}>Get A Proper Nickname!</h1>
       </Grid>
       <Grid container direction ={'row'} justify={'center'} alignItems={'flex-start'}>
         <h1 className={'nickname'}>&nbsp;{nickname}</h1>
--- src/utils.ts
+++ src/utils.ts
@@ -1,38 +1,68 @@
-import * as _ from 'lodash';
-import * as syllable from "syllable";
+import * as _        from 'lodash';
+import * as syllable from 'syllable';
+import {seedList}    from './seedList';
+
+const capitalize = (string: string) => _.startCase(_.camelCase(string));
+
+export const random = (list: any[]) => list[Math.floor(Math.random() * list.length)];
+
+const searchBestDatamuse = (suggestions: any, names: any) => {
+  _.map(suggestions, (suggestion: any) => suggestion.score = syllable(suggestion.word));
+  _.map(suggestions, (suggestion: any) => {
+    if ((suggestion.score + names.first.syllables) % 2 === 0 && capitalize(suggestion.word.substring(0, 2)) === names.first.twoLetters) {
+      return suggestion.top = true;
+    } else if ((suggestion.score + names.first.syllables) % 2 === 0) {
+      return suggestion.bottom = true;
+    } else {
+      return false;
+    }
+  });
+  const best = _.filter(suggestions, (suggestion: any) => suggestion.top === true);
+  const mediocre = _.filter(suggestions, (suggestion: any) => suggestion.bottom === true);
+  if (best.length > 0) {
+    console.log('Best: ' + capitalize(random(best).word));
+    return capitalize(random(best).word);
+  } else if (mediocre.length > 0) {
+    console.log('Mediocre: ' + capitalize(random(mediocre).word));
+    return capitalize(random(mediocre).word);
+  } else if (suggestions.length === 0) {
+    console.log('Special: ');
+    return 'Special';
+  } else {
+    console.log('Fail: ' + capitalize(random(suggestions).word));
+    return capitalize(random(suggestions).word);
+  }
+};
 
 export const arrangeInput = (name: string) => {
   const stringList = name.trim().split(/\s+/);
-  const namesList = _.map(stringList, (name: string) => _.startCase(_.camelCase(name)));
-  const namesInput: any = {first: {name: namesList[0], letter: namesList[0].charAt(0)}};
+  const namesList = _.map(stringList, (name: string) => capitalize(name));
+  const namesInput: any = {first: {name: namesList[0], letter: namesList[0].charAt(0), twoLetters: namesList[0].substring(0, 2)}};
   if (namesList.length > 1) {
-    namesInput.second = {name: namesList.slice(-1)[0], letter: namesList.slice(-1)[0].charAt(0)};
-    namesInput.type = 'middleNick';
-  } else {
-    namesInput.type = 'frontNick';
+    namesInput.second = {name: namesList.slice(-1)[0], letter: namesList.slice(-1)[0].charAt(0), twoLetters: namesList.slice(-1)[0].substring(0, 2)}
   }
   return namesInput;
 };
 
-
 export const arrangeCadence = (names: any) => {
-
-  // add syllable count to the object being built up
-
-
-  console.log(syllable(names['first']['name']));
-
-
+  names.syllables = syllable(names.name);
+  return names;
 };
 
-
-
-// const datamuseRequest = () => {
-//  includes running new words through syllable thingy
-//  pick the first two words that fit the syllable pattern
-// randomly return one or the other with the name
-// }:
-
+export const datamuseRequest = async (names: any, seed: string, count: number) => {
+  const initResponse = await fetch(`https://api.datamuse.com/words?rel_jjb=${seed}&sp=${names.first.letter}*`);
+  const response = await initResponse.json();
+  if (Object.keys(response).length === 0) {
+    count++;
+    if (count <= 25) {
+      await datamuseRequest(names, random(seedList), 0);
+    } else {
+      return 'Special ' + names['first']['name'];
+    }
+  } else {
+    return await searchBestDatamuse(response, names) + ' ' + names.first.name;
+  }
+};
 
 // const urbanDictRequest = () => {
 //  includes running new words through syllable thingy
